472c83374cbf32bfadd22d31f5bcae6f66590446
slifspo <hhsw1606@naver.com>
2020-04-04 23:10:31 +0900

    Set Mongo DB, add env and other settings

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
+require('dotenv').config(); // .env 파일에서 환경변수 불러오기
+
 const Koa = require('koa');
 const Router = require('koa-router');
 
 const app = new Koa();
 const router = new Router();
+const api = require('./api');
 
-router.get('/', (ctx, next) => {
-    ctx.body = '홈';
+const mongoose = require('mongoose');
+const bodyParser = require('koa-bodyparser');
+
+mongoose.Promise = global.Promise; // Node 의 네이티브 Promise 사용
+// mongodb 연결
+mongoose.connect(process.env.MONGO_URI).then(
+    (response) => {
+        console.log('Successfully connected to mongodb');
+    }
+).catch(e => {
+    console.error(e);
 });
 
-app.use(router.routes());
-app.use(router.allowedMethods());
+const port = process.env.PORT || 4000; // PORT 값이 설정되어있지 않다면 4000 을 사용합니다.
 
-app.listen(4000, () => {
-    console.log('heurm server is listening to port 4000');
+app.use(bodyParser()); // 바디파서 적용, 라우터 적용코드보다 상단에 있어야합니다.
+
+router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
+app.use(router.routes()).use(router.allowedMethods());
+
+
+app.listen(port, () => {
+    console.log('bgs server is listening to port ' + port);
 });
