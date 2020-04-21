5b93e90b46d80a93943f6f41dab80877af6b3ec1
slifspo <hhsw1606@naver.com>
2020-04-22 03:07:37 +0900

    Add additional code in emailVerify
    Add verification of request data
    Change method from GET to PATCH
    Add find method to get updated document so that changed token can be created

--- src/api/auth/auth.controller.js
+++ src/api/auth/auth.controller.js
@@ -70,7 +70,7 @@ exports.logout = (ctx) => {
     ctx.status = 204;
 };
 
-// 쿠키에 access_token 이 있다면 복호화하여 현재 로그인된 유저의 정보를 알려줌, user: { _id, profile }
+// 현재 로그인된 유저의 정보를 알려줌, user: { _id, profile }
 exports.check = (ctx) => {
     const { user } = ctx.request;
 
@@ -79,14 +79,29 @@ exports.check = (ctx) => {
         return;
     }
 
+    console.log(user.profile);
+
     ctx.body = user.profile;
 };
 
 // 이메일 인증
-exports.email = async (ctx) => {
-    const { email, key } = ctx.request.query;
-    let account = null;
+exports.emailVerify = async (ctx) => {
+    // 데이터 검증
+    const schema = Joi.object().keys({
+        email: Joi.string().email().required(),
+        key: Joi.string().hex().required()
+    });
 
+    const result = Joi.validate(ctx.request.body, schema);
+
+    if(result.error) {
+        ctx.status = 400; // Bad Request
+        return;
+    }
+
+    const { email, key } = ctx.request.body;
+    
+    let account = null;
     try {
         // 이메일로 유저 인스턴스를 찾음
         account = await Account.findByEmail(email);
@@ -95,24 +110,44 @@ exports.email = async (ctx) => {
     }
 
     // key 일치여부 확인
-    if(account.key_for_verify != key){
+    if(account.email.key_for_verify != key){
         ctx.status = 403; // Forbidden
         return;
     }
-    try { // 일치하면 email_verified 를 true 로 바꿈
-        await account.update({ email_verified: true });
+
+    try { // 일치하면 profile.verified 를 true 로 업데이트
+        await account.update({ 'profile.verified': true });
     } catch (e) {
         ctx.throw(500, e);
     }
-    
-    // 토큰 생성
+
+    // 바뀐 profile의 토큰을 다시 생성
     let token = null;
     try {
+        account = await Account.findByEmail(email); // 업데이트된 document 가져옴
         token = await account.generateToken();
     } catch (e) {
         ctx.throw(500, e);
     }
 
     ctx.cookies.set('access_token', token, { httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * 7 });
-    ctx.body = account.profile; // 프로필 정보로 응답합니다.
+    /* ctx.body = account.profile; // 프로필 정보로 응답합니다. */
+    /* ctx.body = { verified: true }; */
+    ctx.status = 200;
 }
+
+/* // 유저 이메일 인증 여부 확인
+exports.emailVerified = async (ctx) => {
+    const { email } = ctx.params;
+    let account = null;
+
+    try {
+        account = await Account.findByEmail(email);    
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+
+    ctx.body = {
+        verified: account.profile.verified
+    };
+} */
