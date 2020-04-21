5a7871108ca3322b843f29e1f132068d9783af3b
slifspo <hhsw1606@naver.com>
2020-04-22 03:08:29 +0900

    Change emailVerify method from GET to PATCH

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -6,6 +6,7 @@ auth.post('/login/local', authCtrl.localLogin); // 로컬 로그인
 auth.get('/exists/:key(email|username)/:value', authCtrl.exists); // 이메일|아이디 존재유무 확인. key 파라미터가 email 이나 username 일때만 허용
 auth.post('/logout', authCtrl.logout); // 로그아웃
 auth.get('/check', authCtrl.check); // 현재 로그인된 유저의 정보를 알려줌
-auth.get('/email', authCtrl.email); // 이메일 인증
+auth.patch('/verify/email', authCtrl.emailVerify); // 이메일 인증
+/* auth.get('/verified/email/:email', authCtrl.emailVerified); // 유저 이메일 인증 여부 확인 */
 
 module.exports = auth;
