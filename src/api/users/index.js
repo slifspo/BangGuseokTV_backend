3d1ed6a19b52eb6b78a6654a90da1686633c5ea8
slifspo <hhsw1606@naver.com>
2020-04-24 19:46:53 +0900

    Add update username route

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -3,5 +3,6 @@ const users = new Router();
 const usersCtrl = require('./users.controller');
 
 users.post('/register/local', usersCtrl.localRegister); // 로컬 회원가입
+users.patch('/profile/username', usersCtrl.updateUsername); // 유저이름 설정
 
 module.exports = users;
