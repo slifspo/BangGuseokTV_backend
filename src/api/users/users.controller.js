08867de8e8a55b09db7c3faf34d41347eb71ac38
slifspo <hhsw1606@naver.com>
2020-04-24 19:50:13 +0900

    Add update username method
    Add auth validation
    Add data validation
    Add token generation
    Change 'Account' to 'Accounts' for clarity

--- src/api/users/users.controller.js
+++ src/api/users/users.controller.js
@@ -1,5 +1,5 @@
 const Joi = require('joi');
-const Account = require('models/Account');
+const Accounts = require('models/Account');
 
 // 로컬 회원가입
 exports.localRegister = async (ctx) => {
@@ -20,7 +20,7 @@ exports.localRegister = async (ctx) => {
     // 아이디 / 이메일 중복 체크
     let existing = null;
     try {
-        existing = await Account.findByEmailOrUsername(ctx.request.body);
+        existing = await Accounts.findByEmailOrUsername(ctx.request.body);
     } catch (e) {
         ctx.throw(500, e);
     }
@@ -38,7 +38,7 @@ exports.localRegister = async (ctx) => {
     // 계정 생성
     let account = null;
     try {
-        account = await Account.localRegister(ctx.request.body);
+        account = await Accounts.localRegister(ctx.request.body);
     } catch (e) {
         ctx.throw(500, e);
     }
@@ -62,3 +62,77 @@ exports.localRegister = async (ctx) => {
     ctx.cookies.set('access_token', token, { httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * 7 });
     ctx.body = account.profile; // 프로필 정보로 응답합니다.
 };
+
+// 유저이름 설정
+exports.updateUsername = async (ctx) => {
+    const { user } = ctx.request;
+
+    // 권한 검증
+    if(!user) {
+        ctx.status = 403; // Forbidden
+        return;
+    }
+
+    // 유저이름이 null일때만 허용
+    let account = null;
+    try {
+        account = await Accounts.findById(user._id);
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+    if(account.profile.username !== null){
+        ctx.status = 406; // Not allowed
+        return;
+    }
+
+    // 데이터 검증
+    const schema = Joi.object().keys({
+        username: Joi.string().regex(/^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9]+$/).min(3).max(20).required(), // 한글영어숫자 3~20자
+    });
+
+    const result = Joi.validate(ctx.request.body, schema);
+
+    if(result.error) {
+        ctx.status = 400; // Bad request
+        return;
+    }
+
+    const { username } = ctx.request.body;
+
+    // 중복 체크
+    let existing = null;
+    try {
+        existing = await Accounts.findByUsername(username);
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+
+    if(existing) {
+    // 중복되는 유저이름이 있을 경우
+        ctx.status = 409; // Conflict
+        // 어떤 값이 중복되었는지 알려줍니다
+        ctx.body = {
+            key: 'username'
+        };
+        return;
+    }
+
+    // 유저이름 변경
+    try { // 일치하면 profile.verified 를 true 로 업데이트
+        await account.update({ 'profile.username': username });
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+
+    // 바뀐 profile의 토큰을 다시 생성
+    let token = null;
+    try {
+        account = await Accounts.findById(user._id);; // 업데이트된 document 가져옴
+        token = await account.generateToken();
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+
+    ctx.cookies.set('access_token', token, { httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * 7 });
+    ctx.body = account.profile.username; // 유저이름으로 응답.
+}
