f859355937b05da2976105a6fe7a610649c01050
slifspo <hhsw1606@naver.com>
2020-05-04 03:23:12 +0900

    Add rooms api route

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,10 @@ const Router = require('koa-router');
 const api = new Router();
 const auth = require('./auth');
 const users = require('./users');
+const rooms = require('./rooms');
 
 api.use('/auth', auth.routes()); // 인증
 api.use('/users', users.routes()); // 유저
+api.use('/rooms', rooms.routes()); // 방
 
 module.exports = api;
