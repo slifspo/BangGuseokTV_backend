ea39958fab50cfbd50c8f506d3e3a2b297b8fcbe
slifspo <hhsw1606@naver.com>
2020-05-09 02:11:32 +0900

    Add image upload function

--- src/api/rooms/rooms.controller.js
+++ src/api/rooms/rooms.controller.js
@@ -1,5 +1,8 @@
 const Joi = require('joi');
+const path = require('path');
+const moment = require('moment');
 const Rooms = require('models/room');
+const { mkdirFile, saveFile } = require('lib/upload');
 
 // 방 목록 조회, 한번에 최대 12개씩
 exports.getRooms = async (ctx) => {
@@ -25,4 +28,27 @@ exports.getRooms = async (ctx) => {
     }
 
     ctx.body = rooms;
+};
+
+// 방 이미지 업로드
+exports.imgUpload = async (ctx) => {
+    let date = moment().format('YYYYMMDDHHmmss')
+    let file = ctx.request.files.file;
+    let fileName = date + "_" + file.name // 파일 이름
+    let filePath = path.join(__dirname, '../../../public/uploads'); // 파일 경로
+
+    await mkdirFile(filePath); // 경로 생성
+    await saveFile(file.path, filePath + '\\' + fileName).then(path => {
+        //console.log(path);
+        ctx.body = {
+            error_code: 10000,
+            error_message: 'Successful upload of files',
+        }
+    }).catch(err => {
+        console.log(err);
+        ctx.body = {
+            error_code: 20008,
+            error_message: 'Failed to upload file!'
+        }
+    })
 };
