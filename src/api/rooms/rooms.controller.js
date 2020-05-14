e82fab472802e4b6628a8aff22b411e63099871c
slifspo <hhsw1606@naver.com>
2020-05-15 02:44:24 +0900

    Change method of uploading image file
    Add auth verification
    Comment unused lines
    Change upload method from saving into static folder to update DB

--- src/api/rooms/rooms.controller.js
+++ src/api/rooms/rooms.controller.js
@@ -1,8 +1,9 @@
 const Joi = require('joi');
 const path = require('path');
-const moment = require('moment');
+//const moment = require('moment');
 const Rooms = require('models/room');
-const { mkdirFile, saveFile } = require('lib/upload');
+const fs = require('fs');
+//const { mkdirFile, saveFile } = require('lib/upload');
 
 // 방 목록 조회, 한번에 최대 12개씩
 exports.getRooms = async (ctx) => {
@@ -30,15 +31,30 @@ exports.getRooms = async (ctx) => {
     ctx.body = rooms;
 };
 
-// 방 이미지 업로드
+// 방 이미지 DB에 업로드
 exports.imgUpload = async (ctx) => {
-    let date = moment().format('YYYYMMDDHHmmss')
-    let file = ctx.request.files.file;
-    let fileName = date + "_" + file.name // 파일 이름
-    let filePath = path.join(__dirname, '../../../public/uploads'); // 파일 경로
+    const { user } = ctx.request;
 
-    await mkdirFile(filePath); // 경로 생성
-    await saveFile(file.path, filePath + '\\' + fileName).then(path => {
+    // 권한 검증
+    if (!user) {
+        ctx.status = 403; // Forbidden
+        return;
+    }
+
+    //const date = moment().format('YYYYMMDDHHmmss')
+    const file = ctx.request.files.file;
+    const ext = path.extname(file.path);
+    const fileName = user.profile.username + ext; // 파일 이름
+    const filePath = path.join(process.env.PUBLICPATH, 'roomImg'); // 파일 경로
+
+    // 확장자가 없는경우 + 파일 type 검사하기도 추가하기
+    if (ext === '') {
+        ctx.status = 406; // Not allowed type
+        return;
+    }
+
+/*    await mkdirFile(filePath); // 경로 생성
+     await saveFile(file.path, filePath + '\\' + fileName).then(path => {
         //console.log(path);
         ctx.body = {
             error_code: 10000,
@@ -50,5 +66,62 @@ exports.imgUpload = async (ctx) => {
             error_code: 20008,
             error_message: 'Failed to upload file!'
         }
-    })
+        return;
+    }) */
+
+    // 해당 유저의 방 찾기
+    let room = null;
+    try {
+        room = await Rooms.findByUserId(user._id);
+    } catch {
+        ctx.throw(500, e);
+        return;
+    }
+
+    // thumbnail 업데이트
+    const imgData = fs.readFileSync(file.path);
+    const contentType = file.type;
+    try {
+        await room.update({
+            'profile.thumbnail.data': imgData,
+            'profile.thumbnail.contentType': contentType
+        });
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+
+/*     // thumbnail 경로 업데이트
+    try {
+        await room.update({ 'profile.thumbnail': 'roomImg/' + fileName });
+    } catch (e) {
+        ctx.throw(500, e);
+    } */
+
+    /* const fs = require('fs');
+    const img = fs.readFileSync(filePath+'\\'+fileName);
+    ctx.response.set("content-type", "image/jpeg");
+    ctx.body = img; */
+    ctx.status = 204; // No contents
 };
+
+/* Stream을 response에 보내는 방법 */
+/* const readable = require('stream').Readable
+const s = new readable;
+
+// response
+app.use(ctx => {
+    if (ctx.request.url === '/stream') {
+        // stream data
+        s.push('STREAM: Hello, World!');
+        s.push(null); // indicates end of the stream
+        ctx.body = s;
+    } else if (ctx.request.url === '/file') {
+        // stream file
+        const src = fs.createReadStream('./big.file');
+        ctx.response.set("content-type", "txt/html");
+        ctx.body = src;
+    } else {
+        // normal KOA response
+        ctx.body = 'BODY: Hello, World!' ;
+    }
+}); */
