6ad3a35d41c7355a400285ee481e81af23e0a0fb
slifspo <hhsw1606@naver.com>
2020-05-15 19:22:15 +0900

    Change method namae and Add getUserRoom router

--- src/api/rooms/index.js
+++ src/api/rooms/index.js
@@ -3,6 +3,7 @@ const rooms = new Router();
 const roomsCtrl = require('./rooms.controller');
 
 rooms.get('/', roomsCtrl.getRooms); // 방 목록 조회, 한번에 최대 12개씩
-rooms.patch('/profile/thumbnail', roomsCtrl.updateThumbnail); // 이미지 업로드, thumbnail 경로 수정
+rooms.patch('/profile/thumbnail', roomsCtrl.updateThumbnail); // 방 이미지 DB에 업로드
+rooms.get('/:username', roomsCtrl.getUserRoom); // 방 이미지 검색
 
 module.exports = rooms;
