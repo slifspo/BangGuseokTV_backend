076fad6aeea66d7b0b32fa199459685827ddefcd
slifspo <hhsw1606@naver.com>
2020-05-16 00:22:58 +0900

    Add updateProfile router

--- src/api/rooms/index.js
+++ src/api/rooms/index.js
@@ -5,5 +5,6 @@ const roomsCtrl = require('./rooms.controller');
 rooms.get('/', roomsCtrl.getRooms); // 방 목록 조회, 한번에 최대 12개씩
 rooms.patch('/profile/thumbnail', roomsCtrl.updateThumbnail); // 방 이미지 DB에 업로드
 rooms.get('/:username', roomsCtrl.getUserRoom); // 방 이미지 검색
+rooms.patch('/profile', roomsCtrl.updateProfile); // 방 설정 업데이트
 
 module.exports = rooms;
