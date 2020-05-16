9d10f7dad1e5d5f7fe98868a59df3bc318c852cf
slifspo <hhsw1606@naver.com>
2020-05-16 19:34:00 +0900

    Add avatar update router

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -4,5 +4,6 @@ const usersCtrl = require('./users.controller');
 
 users.post('/register/local', usersCtrl.localRegister); // 로컬 회원가입
 users.patch('/profile/username', usersCtrl.updateUsername); // 유저이름 설정
+users.patch('/profile/avatar', usersCtrl.updateAvatar); // 아바타 업데이트
 
 module.exports = users;
