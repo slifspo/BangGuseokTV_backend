0f63ad548da1bece557e583d5dacbcef7c46d25b
slifspo <hhsw1606@naver.com>
2020-05-26 22:50:06 +0900

    Add routers for playlists

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -5,5 +5,7 @@ const usersCtrl = require('./users.controller');
 users.post('/register/local', usersCtrl.localRegister); // 로컬 회원가입
 users.patch('/profile/username', usersCtrl.updateUsername); // 유저이름 설정
 users.patch('/profile/avatar', usersCtrl.updateAvatar); // 아바타 업데이트
+users.post('/profile/playlists/videos', usersCtrl.addToPlaylist); // 재생목록에 항목 추가
+users.get('/profile/playlists', usersCtrl.getPlaylists); // 재생목록 가져오기
 
 module.exports = users;
