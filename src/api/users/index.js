6e89a9a480e6d5f9f114c7ae5d32e25785d1a4bb
slifspo <hhsw1606@naver.com>
2020-05-30 18:21:36 +0900

    Add router

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -6,6 +6,7 @@ users.post('/register/local', usersCtrl.localRegister); // 로컬 회원가입
 users.patch('/profile/username', usersCtrl.updateUsername); // 유저이름 설정
 users.patch('/profile/avatar', usersCtrl.updateAvatar); // 아바타 업데이트
 users.post('/profile/playlists/videos', usersCtrl.addToPlaylist); // 재생목록에 항목 추가
+users.delete('/profile/playlists/videos', usersCtrl.removeFromPlaylist); // 재생목록에 항목 제거
 users.get('/profile/playlists', usersCtrl.getPlaylists); // 재생목록 가져오기
 users.get('/profile/playlists/videos', usersCtrl.getPlaylistVideos); // 재생목록의 동영상 가져오기
 
