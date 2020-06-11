a8f4116016759653f40423606f10930f63ae7b4c
slifspo <hhsw1606@naver.com>
2020-06-11 20:56:21 +0900

    Add join playlist route

--- src/api/rooms/index.js
+++ src/api/rooms/index.js
@@ -6,5 +6,6 @@ rooms.get('/', roomsCtrl.getRooms); // 방 목록 조회, 한번에 최대 12개
 rooms.patch('/profile/thumbnail', roomsCtrl.updateThumbnail); // 방 이미지 DB에 업로드
 rooms.get('/:username', roomsCtrl.getUserRoom); // 방 이미지 검색
 rooms.patch('/profile', roomsCtrl.updateProfile); // 방 설정 업데이트
+rooms.post('/playerlist', roomsCtrl.joinPlayerlist); // playerlist 추가
 
 module.exports = rooms;
