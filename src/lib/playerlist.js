3ee1836ec8147a2dd1ed580a6d5ccf6b42d12b32
slifspo <hhsw1606@naver.com>
2020-06-13 01:30:04 +0900

    Add getting youtube video duration to set timer

--- src/lib/playerlist.js
+++ src/lib/playerlist.js
@@ -3,7 +3,14 @@ const Accounts = require('models/account');
 
 let isPlaying = {};
 
-const startPlayerlist = async (io, hostname, room_id) => {
+// convert ISO 8601 duration (ms)
+const convertTime = (youtube_time) => {
+    array = youtube_time.match(/(\d+)(?=[MHS])/ig) || [];
+    return (parseInt(array[0]) * 60 + parseInt(array[1])) * 1000;
+}
+
+const startPlayerlist = async (ctx, hostname, room_id) => {
+    const { io } = ctx;
     // playerlist 실행중으로 바꾸기
     isPlaying[hostname] = [true, null];
 
@@ -21,12 +28,12 @@ const startPlayerlist = async (io, hostname, room_id) => {
 
     // playerlist 에 아무도 없을 때
     if (firstPlayer === undefined) {
-        //console.log(hostname + ' 방의 playerlist 멈춤');
+        console.log(hostname + ' 방의 playerlist 멈춤');
         // playerlist 정지
         isPlaying[hostname][0] = false;
 
         // 빈 문자열을 emit 해서 클라이언트의 player 정지
-        io.to(hostname).emit('sendVideoId', {videoId: ''});
+        io.to(hostname).emit('sendVideoId', { videoId: '' });
         return;
     }
 
@@ -57,14 +64,36 @@ const startPlayerlist = async (io, hostname, room_id) => {
     const selectedPlaylist = account.selectedPlaylist;
     const firstVideo = account.playlists[selectedPlaylist].videos[0];
 
+    let videoDuration = null; // 비디오 재생시간
     // firstPlayer의 firstVideo가 있으면
     if (firstVideo !== undefined) {
-        //console.log(firstPlayer.username + ' 의 playlist \'' + account.playlists[selectedPlaylist].name + '\' 의 ' + firstVideo.videoTitle + ' 이 재생중');
+        console.log(firstPlayer.username + ' 의 playlist \'' + account.playlists[selectedPlaylist].name + '\' 의 ' + firstVideo.videoTitle + ' 이 재생중');
 
         // videoId를 emit
-        io.to(hostname).emit('sendVideoId', {videoId: firstVideo.videoId});
+        io.to(hostname).emit('sendVideoId', { videoId: firstVideo.videoId });
 
         // YouTube Data API 로 video duration 얻기
+        const optionParams = { // 검색 파라미터
+            id: firstVideo.videoId,
+            part: "contentDetails",
+            key: process.env.GOOGLE_API_KEY
+        };
+        let url = "https://www.googleapis.com/youtube/v3/videos?"; // URL 생성
+        for (let option in optionParams) {
+            url += option + "=" + optionParams[option] + "&";
+        }
+        url = url.substr(0, url.length - 1); //url의마지막에 붙어있는 & 정리
+    
+        // http 요청
+        let res = null;
+        try {
+            res = await ctx.get(url);
+        } catch (e) {
+            ctx.throw(500, e);
+        }
+
+        // 유튜브 video 의 재생시간 변환
+        videoDuration = convertTime(res.items[0].contentDetails.duration);
 
         // firstVideo를 배열에서 pop
         try {
@@ -122,9 +151,13 @@ const startPlayerlist = async (io, hostname, room_id) => {
         // io.emit 으로 대기열에서 나가졌다고 알리기
     }
 
-    // 재귀호출
-    const timerObj = setTimeout(startPlayerlist, 3000, io, hostname, room_id);
+    videoDuration = (videoDuration === null) ? 2000 : videoDuration;
+    console.log(videoDuration);
+    // 재귀타이머 설정
+    const timerObj = setTimeout(startPlayerlist, videoDuration, ctx, hostname, room_id);
     isPlaying[hostname][1] = timerObj;
+/*     const { getTimeLeft } = require('lib/timer');
+    setInterval(getTimeLeft, 1000, timerObj); */
 };
 module.exports = {
     isPlaying, startPlayerlist
