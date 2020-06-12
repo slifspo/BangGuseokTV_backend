015d95b1fff9042fd1e1cc287145c5e0e096c78d
slifspo <hhsw1606@naver.com>
2020-06-12 22:31:41 +0900

    Modify route URI and add updateSelectedPlaylist route

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -5,11 +5,12 @@ const usersCtrl = require('./users.controller');
 users.post('/register/local', usersCtrl.localRegister); // 로컬 회원가입
 users.patch('/profile/username', usersCtrl.updateUsername); // 유저이름 설정
 users.patch('/profile/avatar', usersCtrl.updateAvatar); // 아바타 업데이트
-users.post('/profile/playlists/videos', usersCtrl.addToPlaylist); // 재생목록에 항목 추가
-users.delete('/profile/playlists/videos', usersCtrl.removeFromPlaylist); // 재생목록에 항목 제거
-users.get('/profile/playlists', usersCtrl.getPlaylists); // 재생목록 가져오기
-users.get('/profile/playlists/videos', usersCtrl.getPlaylistVideos); // 재생목록의 동영상 가져오기
-users.post('/profile/playlists', usersCtrl.addPlaylist); // 새로운 재생목록 추가
-users.delete('/profile/playlists', usersCtrl.removePlaylist); // 재생목록에 항목 제거
+users.get('/playlists', usersCtrl.getPlaylists); // 재생목록 가져오기
+users.post('/playlists', usersCtrl.addPlaylist); // 새로운 재생목록 추가
+users.delete('/playlists', usersCtrl.removePlaylist); // 재생목록에 항목 제거
+users.get('/playlists/videos', usersCtrl.getPlaylistVideos); // 재생목록의 동영상 가져오기
+users.post('/playlists/videos', usersCtrl.addToPlaylist); // 재생목록에 항목 추가
+users.delete('/playlists/videos', usersCtrl.removeFromPlaylist); // 재생목록에 항목 제거
+users.patch('/selectedPlaylist', usersCtrl.updateSelectedPlaylist); // 선택된 재생목록 설정
 
 module.exports = users;
