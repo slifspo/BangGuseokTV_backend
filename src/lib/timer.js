ea459f1825c4a7a6890c645ea56d87eb64ebbf97
slifspo <hhsw1606@naver.com>
2020-06-13 01:31:39 +0900

    Add debug code

--- src/lib/timer.js
+++ src/lib/timer.js
@@ -1,4 +1,6 @@
 const getTimeLeft = timeout => {
-    return Math.ceil((timeout._idleStart + timeout._idleTimeout) / 1000 - process.uptime());
+    const timeleft = Math.ceil((timeout._idleStart + timeout._idleTimeout) / 1000 - process.uptime());
+    //console.log(timeleft);
+    return timeleft;
 }
 exports.getTimeLeft = getTimeLeft;
