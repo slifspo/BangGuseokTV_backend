bf5608c525bb53ba0525b350edba45ad2112d5c6
slifspo <hhsw1606@naver.com>
2020-06-13 22:59:31 +0900

    Add hostname param to route

--- src/api/rooms/index.js
+++ src/api/rooms/index.js
@@ -6,7 +6,8 @@ rooms.get('/', roomsCtrl.getRooms); // 방 목록 조회, 한번에 최대 12개
 rooms.patch('/profile/thumbnail', roomsCtrl.updateThumbnail); // 방 이미지 DB에 업로드
 rooms.get('/:username', roomsCtrl.getUserRoom); // 방 이미지 검색
 rooms.patch('/profile', roomsCtrl.updateProfile); // 방 설정 업데이트
-rooms.post('/playerlist', roomsCtrl.joinPlayerlist); // playerlist 추가
-rooms.delete('/playerlist', roomsCtrl.leavePlayerlist); // playerlist 제거
+rooms.post('/:hostname/playerlist', roomsCtrl.joinPlayerlist); // playerlist 추가
+rooms.delete('/:hostname/playerlist', roomsCtrl.leavePlayerlist); // playerlist 제거
+rooms.get('/:hostname/playstate', roomsCtrl.getPlayState); // playState 얻기
 
 module.exports = rooms;
