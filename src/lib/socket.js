ae62b794d46669a473ceac0dad1e231056283642
slifspo <hhsw1606@naver.com>
2020-06-14 03:15:13 +0900

    Add leaving playerlist when socket is disconnected

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,4 +1,5 @@
 const Rooms = require('models/room');
+const { playState } = require('lib/playerlist');
 
 module.exports = (io) => {
     // 소켓 이벤트 정의
@@ -25,11 +26,25 @@ module.exports = (io) => {
     io.on('disconnect', async (ctx, data) => {
         //console.log('클라이언트 연결해제: ' + ctx.socket.id);
 
+        // socket id 가 일치하는 room 을 검색
+        let room = null;
+        try {
+            room = await Rooms.findOne({
+                'playerlist.socketId': ctx.socket.id
+            })
+        } catch (e) {
+            console.log(e);
+            return;
+        }
+
+        if (room === null) return;
+        const { hostname } = room;
+
         // playerlist 에서 제거
         try {
             await Rooms.updateOne(
                 {
-
+                    'hostname': hostname
                 },
                 {
                     '$pull': {
@@ -41,6 +56,28 @@ module.exports = (io) => {
             );
         } catch (e) {
             console.log(e);
+            return;
+        }
+
+        // playerlist 에 한명이라도 있는지 확인
+        try {
+            room = await Rooms.findOne({
+                'hostname': hostname,
+            })
+        } catch (e) {
+            console.log(e);
+            return;
+        }
+        if (room.playerlist[0] === undefined) { // playerlist 가 비어있으면
+            if (playState[hostname] !== undefined) {
+                clearTimeout(playState[hostname][1]); // 타이머 해제
+                playState[hostname] = [false, null, '', '', null]; // 초기화
+                io.to(hostname).emit('sendPlayState', { // 클라이언트 playState 초기화
+                    username: '',
+                    videoId: '',
+                    videoDuration: null
+                });
+            }
         }
     })
 }
