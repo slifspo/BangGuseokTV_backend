549df5b269d87225e7e171666ecdc13813538b9c
slifspo <hhsw1606@naver.com>
2020-10-05 01:32:47 +0900

    Change callback URL

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -15,7 +15,7 @@ module.exports = () => {
     passport.use(new FacebookStrategy({
         clientID: process.env.FACEBOOK_ID,
         clientSecret: process.env.FACEBOOK_SECRET,
-        callbackURL: 'http://localhost:3000/api/auth/login/facebook/callback',
+        callbackURL: 'https://slifspo.github.io/BangGuseokTV_frontend/api/auth/login/facebook/callback',
         session: false, // 세션 사용안함
         profileFields: ['id', 'email', 'displayName']
     }, (accessToken, refreshToken, profile, done) => {
@@ -26,7 +26,7 @@ module.exports = () => {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_ID,
         clientSecret: process.env.GOOGLE_SECRET,
-        callbackURL: 'http://localhost:3000/api/auth/login/google/callback',
+        callbackURL: 'https://slifspo.github.io/BangGuseokTV_frontend/api/auth/login/google/callback',
         session: false, // 세션 사용안함
         profileFields: ['id', 'email', 'displayName']
     }, (accessToken, refreshToken, profile, cb) => {
