b8fb2b04a0554f43583f3c91ca22410819975287
slifspo <hhsw1606@naver.com>
2020-10-16 05:01:42 +0900

    Remove logging

--- src/api/users/users.controller.js
+++ src/api/users/users.controller.js
@@ -582,7 +582,7 @@ exports.addFriendlist = async (ctx) => {
     const { friendname } = ctx.request.body;
 
     // 권한 검증
-    if (!user || user.profile.username != username) {
+    if (!user) {
         ctx.status = 403; // Forbidden
         return;
     }
@@ -769,10 +769,6 @@ exports.deleteSentFriendRequest = async (ctx) => {
     const { user } = ctx.request;
     const { username, friendname } = ctx.params;
 
-    console.log("deleteSentFriendRequest");
-    console.log(username);
-    console.log(friendname);
-
     // 권한 검증
     if (!user) {
         ctx.status = 403; // Forbidden
@@ -787,8 +783,6 @@ exports.deleteSentFriendRequest = async (ctx) => {
         ctx.throw(500, e);
         return;
     }
-
-    console.log(friendAccount._id);
 
     // 친구 추가 요청 제거
     try {
@@ -841,10 +835,6 @@ exports.deleteReceivedFriendRequest = async (ctx) => {
     const { user } = ctx.request;
     const { username, friendname } = ctx.params;
 
-    console.log("deleteReceivedFriendRequest");
-    console.log(username);
-    console.log(friendname);
-
     // 권한 검증
     if (!user) {
         ctx.status = 403; // Forbidden
@@ -859,8 +849,6 @@ exports.deleteReceivedFriendRequest = async (ctx) => {
         ctx.throw(500, e);
         return;
     }
-
-    console.log(friendAccount._id);
 
     // 친구 추가 요청 제거
     try {
