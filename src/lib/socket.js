ae92ce01db9e755d6d340ae4f912d9043d5587d4
slifspo <hhsw1606@naver.com>
2020-11-25 00:29:16 +0900

    Fix wrong socket id refer

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -199,10 +199,7 @@ module.exports.init = (io) => {
 
             console.log(playInfo)
 
-            // user 의 socket id 가져옴
-            const userSocketId = loginUsername.get(username);
-
-            console.log('userSocketId: ' + userSocketId);
+            console.log('userSocketId: ' + socket.id);
 
             // playInfo 가 있다면
             if (playInfo !== undefined) {
@@ -214,7 +211,7 @@ module.exports.init = (io) => {
                     console.log('videoId: ' + playInfo.videoId);
                     console.log('videoDuration: ' + playInfo.videoDuration);
 
-                    io.to(userSocketId).emit('sendPlayInfo', {
+                    io.to(socket.id).emit('sendPlayInfo', {
                         sort: sort,
                         username: playInfo.username,
                         videoId: playInfo.videoId,
@@ -228,7 +225,7 @@ module.exports.init = (io) => {
                     console.log('playback: ' + (playInfo.videoDuration - timeleft));
                     
                     // 해당 유저에게 현재 재생시간 보냄
-                    io.to(userSocketId).emit('sendPlayInfo', {
+                    io.to(socket.id).emit('sendPlayInfo', {
                         sort: sort,
                         playback: playInfo.videoDuration - timeleft,
                     });
