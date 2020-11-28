b1edf4766cb15d3d65b69ca98e8ce0a944843d42
slifspo <hhsw1606@naver.com>
2020-11-28 21:45:09 +0900

    Remove and trim unused code

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
-require('dotenv').config(); // .env 파일에서 환경변수 불러오기
+// .env 파일에서 환경변수 불러오기
+require('dotenv').config();
 
+// 모듈 불러오기
 const Koa = require('koa');
-const IO = require('koa-socket-2');
-const cors = require('@koa/cors');
 const Router = require('koa-router');
-const session = require('koa-session');
+const IO = require('koa-socket-2');
 
-
-const app = new Koa();
-const io = new IO();
-
-const router = new Router();
-const api = require('./api');
-
-const mongoose = require('mongoose');
+const cors = require('@koa/cors');
 const bodyParser = require('koa-bodyparser');
-const { jwtMiddleware } = require('lib/token');
-
-const { passport } = require('lib/passport');
-//const passportConfig = require('lib/passport');
-
 const koaBody = require('koa-body')
 const serve = require('koa-static');
+
+const mongoose = require('mongoose');
 const path = require('path');
 
-mongoose.Promise = global.Promise; // Node 의 네이티브 Promise 사용
+const { jwtMiddleware } = require('lib/token');
+const { passport } = require('lib/passport');
+
+// 인스턴스 생성
+const app = new Koa();
+const router = new Router();
+const io = new IO();
+
+// API 경로
+const api = require('./api');
+
+// Node 의 네이티브 Promise 사용
+mongoose.Promise = global.Promise;
 // mongodb 연결
 mongoose.connect(process.env.MONGO_URI).then(
     (response) => {
@@ -34,22 +36,24 @@ mongoose.connect(process.env.MONGO_URI).then(
     console.error(e);
 });
 
-const port = process.env.PORT || 4000; // PORT 값이 설정되어있지 않다면 4000 을 사용합니다.
+// PORT 값이 설정되어있지 않다면 4000 을 사용합니다.
+const port = process.env.PORT || 4000;
 
+// CORS 옵션 설정
 let corsOptions = {
     origin: process.env.CLIENT_HOST, // 허락하고자 하는 요청 주소
-    credentials: true,
-    allowedHeaders: "Content-Type, Authorization",
-} 
+    credentials: true, // 쿠키나 자격증명 정보를 인식
+}
 
 // CORS 허용
 app.proxy = true;
-app.use(session({ sameSite: 'none', secure: true }, app));
 app.use(cors(corsOptions));
 
-app.use(serve(path.join(__dirname, '../public'))); // public폴더에서 정적 파일 제공
+// public폴더에서 정적 파일 제공
+app.use(serve(path.join(__dirname, '../public')));
 
-app.use(koaBody({ // formdata parse
+// formdata parse
+app.use(koaBody({
     multipart: true,
     urlencoded: true,
     formidable: {
@@ -58,18 +62,25 @@ app.use(koaBody({ // formdata parse
     },
     formLimit: '5mb'
 }));
-app.use(bodyParser()); // 바디파서 적용, 라우터 적용코드보다 상단에 있어야합니다.
-app.use(jwtMiddleware); // JWT 처리 미들웨어 적용
 
-app.use(passport.initialize()); // passport 구동
+// 바디파서 적용, 라우터 적용코드보다 상단에 있어야합니다.
+app.use(bodyParser());
+
+// JWT 처리 미들웨어 적용
+app.use(jwtMiddleware);
+
+// passport 구동
+app.use(passport.initialize());
 app.use(passport.session());
 require('lib/passport').init();
 
-io.attach(app); // socket.io
-require('lib/socket').init(io); // socket event 정의
-app.context.io = io; // context(ctx) 에 io를 attach
+// socket.io 를 연결
+io.attach(app);
+// socket event 정의
+require('lib/socket').init(io);
 
-router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
+// api 라우트를 ./api 경로 하위 라우트로 설정
+router.use('/api', api.routes()); 
 app.use(router.routes())
 app.use(router.allowedMethods());
 
