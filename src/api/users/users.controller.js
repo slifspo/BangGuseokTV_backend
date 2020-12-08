7db8bb97ad13904fee64f1947d4b39d74bcef833
slifspo <hhsw1606@naver.com>
2020-12-09 01:45:09 +0900

    Modify authority

--- src/api/users/users.controller.js
+++ src/api/users/users.controller.js
@@ -580,7 +580,7 @@ exports.addReceivedFriendRequests = async (ctx) => {
     const { friendname } = ctx.request.body; // 친구추가 요청 보내는유저
 
     // 권한 검증
-    if (!user || user.profile.username != username) {
+    if (!user) {
         ctx.status = 403; // Forbidden
         return;
     }
